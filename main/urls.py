710361d2b915cf1692493febffd492a4f64d5f75
astzkkk99 <asthazhorif@gmail.com>
2024-11-14 16:01:39 +0700

    refactor: changed landing page url to ''

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,6 @@ from main.views import landing_page, show_main
 app_name = 'main'
 
 urlpatterns = [
-    path("landingpage/", landing_page, name="landing_page"),
+    path("", landing_page, name="landing_page"),
     path("homepage/", show_main, name="show_main"),
 ]
